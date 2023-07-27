b4090e332f9f90df5f1fd5aefd20ef7763fb62a4
OiJohnSteven <oisteven018@gmail.com>
2023-07-27 19:42:16 +0300

    UI Design

---
+++ app/src/main/java/com/ivanojok/mycalc/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ivanojok.mycalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        /**
+         * Create a UI design with two edit texts (to capture to doubles)
+         * And four buttons for the operations ( +, -, *, /)
+         * Also add a text view that will display the result
+         *
+         *
+         * */
+    }
+}
