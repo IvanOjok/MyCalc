e50f83688d3d1a68fecd8ff621784d52568770fa
OiJohnSteven <oisteven018@gmail.com>
2023-08-03 19:04:21 +0300

    User experience

--- app/src/main/java/com/ivanojok/mycalc/MainActivity.kt
+++ app/src/main/java/com/ivanojok/mycalc/MainActivity.kt
@@ -2,19 +2,56 @@ package com.ivanojok.mycalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** image view */
+        val image = findViewById<ImageView>(R.id.my_image)
+        /** edit texts */
+        val firstNumberEditText = findViewById<EditText>(R.id.first_number)
+        val secondNumberEditText:EditText = findViewById(R.id.second_number)
 
-        /**
-         * Create a UI design with two edit texts (to capture to doubles)
-         * And four buttons for the operations ( +, -, *, /)
-         * Also add a text view that will display the result
-         *
-         *
-         * */
+        /** buttons */
+        val sumButton = findViewById<Button>(R.id.sum_button)
+        val subtractButton = findViewById<Button>(R.id.subtract_button)
+        val divideButton = findViewById<Button>(R.id.division_button)
+        val multiplyButton = findViewById<Button>(R.id.multiply_button)
+
+        /** text view */
+        val answerText:TextView = findViewById(R.id.answer)
+
+
+
+        sumButton.setOnClickListener {
+            val firstNumber = firstNumberEditText.text.toString().toInt()
+            val secondNumber = secondNumberEditText.text.toString().toDouble()
+
+            val sum = firstNumber + secondNumber
+            //answerText.setText("The sum is $sum")
+            answerText.text = "The sum is $sum"
+        }
+
+        subtractButton.setOnClickListener {
+            val firstNumber = firstNumberEditText.text.toString().toInt()
+            val secondNumber = secondNumberEditText.text.toString().toDouble()
+
+            val sum = firstNumber - secondNumber
+            //answerText.setText("The sum is $sum")
+            answerText.text = "The difference is $sum"
+        }
+
+
+        answerText.setOnClickListener {
+            val imageArray = arrayOf(R.drawable.chicken_leg, R.drawable.hook, R.drawable.biryani)
+            image.setImageResource(imageArray.random())
+        }
+
     }
 }
